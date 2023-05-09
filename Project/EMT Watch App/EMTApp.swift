f216b186d8ad563f6895197e963d7e7bdbe60d12
Emilio Peláez <me@epelaez.net>
2023-05-09 23:30:18 +0200

    Project Setup

---
+++ Project/EMT Watch App/EMTApp.swift
@@ -0,0 +1,17 @@
+//
+//  EMTApp.swift
+//  EMT Watch App
+//
+//  Created by Emilio Peláez on 9/5/23.
+//
+
+import SwiftUI
+
+@main
+struct EMT_Watch_AppApp: App {
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+}
